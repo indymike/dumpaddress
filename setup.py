af11998e7255b06686ddbe5eeaf280a062e41a44
Mike Seidle <mike@seidle.net>
2014-10-21 22:33:45 -0400

    Updated name to yogurt.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='getum',
+    name='yogurt',
     version='0.1',
     description='Command line tool for exctracting email addresses and urls from text files and links from html files.',
     author='Mike Seidle',
     author_email='mike@seidle.net',
-    download_url='https://github.com/indymike/getum',
-    py_modules=['getum'],
+    download_url='https://github.com/indymike/yogurt',
+    py_modules=['yogurt'],
     install_requires=[
         'Click',
     ],
